850503d32aa3148bfd9c1a618423c73d9b518221
Milad Moulayi <m.moulayi123@gmail.com>
2022-12-11 15:51:36 -0800

    Completed forms, template, and views

--- todo_site/todo_app/views.py
+++ todo_site/todo_app/views.py
@@ -1,12 +1,39 @@
 from django.shortcuts import render
 from django.views import View
+from todo_app.models import Task
+from todo_app.forms import TaskForm
 
 # Create your views here.
 
 class HomeView(View):
     def get(self, request):
+        tasks = Task.objects.all()
+        task_form = TaskForm
+
+        html_data = {
+            'task_list': tasks,
+            'form': task_form,
+        }
+
         return render(
             request=request,
             template_name='index.html',
-            context={}
+            context=html_data,
+        )
+
+    def post(self, request): 
+        task_form = TaskForm(request.POST)
+        task_form.save()
+
+        tasks = Task.objects.all()
+
+        html_data = {
+            'task_list': tasks,
+            'form': task_form,
+        }
+
+        return render(
+            request=request,
+            template_name='index.html',
+            context=html_data,
         )
