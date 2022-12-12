159bd75e73104a230c22dbfac6995dda793ab09e
Milad Moulayi <m.moulayi123@gmail.com>
2022-12-11 17:23:34 -0800

    Added update and delete functionality

--- todo_site/todo_app/views.py
+++ todo_site/todo_app/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import redirect, render
 from django.views import View
 from todo_app.models import Task
 from todo_app.forms import TaskForm
@@ -25,15 +25,32 @@ class HomeView(View):
         task_form = TaskForm(request.POST)
         task_form.save()
 
-        tasks = Task.objects.all()
+        return redirect('home')
+
+class TaskDetailView(View):
+    def get(self, request, task_id):
+        task = Task.objects.get(id=task_id)
+
+        task_form = TaskForm(instance=task)
 
         html_data = {
-            'task_list': tasks,
+            'task_object': task,
             'form': task_form,
         }
 
         return render(
             request=request,
-            template_name='index.html',
+            template_name='detail.html',
             context=html_data,
         )
+
+    def post(self, request, task_id):
+        task = Task.objects.get(id=task_id)
+
+        if 'update' in request.POST:
+            task_form = TaskForm(request.POST, instance=task)
+            task_form.save()
+        elif 'delete' in request.POST:
+            task.delete()
+
+        return redirect('home')
